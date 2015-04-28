0452ec7cd82c62b020fca19b47734bf9e09bffc3
Tony Zhou <tonyzhou@live.cn>
2015-04-28 14:46:05 +0800

    实现了访问相对目录文件的功能

--- OpencartAttack/App/controller/Analysis.py
+++ OpencartAttack/App/controller/Analysis.py
@@ -3,7 +3,16 @@ Created on Apr 26, 2015
 
 @author: tonyzhou
 '''
+import os
 
+#返回指定配置文件的内容
+def getconfigRoute():
+    currentRoute=os.path.dirname(os.path.dirname(__file__))  
+    route=currentRoute[:-4]+"\conf\config.txt"
+    return route
+    
 if __name__ == '__main__':
-    print "hello_app"
-    print "qwerfdsa,god"
+    guide=getconfigRoute()
+    conf=open(guide,'r')
+    words=conf.read();
+    print words
